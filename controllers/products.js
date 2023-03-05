eda79e8185ef7f94854ca985dcd26d344846f98d
UBoiii <der_uboiii@outlook.com>
2023-03-05 18:37:55 +0100

    Added Patch One for products

--- controllers/products.js
+++ controllers/products.js
@@ -2,6 +2,38 @@ const firestoreClientLib = require("firebase/firestore");
 const {firebase} = require("../config/firebase-client-config.js");
 
 const httpStatus = require("http-status-codes");
+const errors = require("../errors");
+
+const patchOne = async (req, res) => {
+    const uid = req.uid;
+    const prid = req.params.id;
+    const db = firestoreClientLib.getFirestore(firebase);
+    const prSnapshot = await firestoreClientLib.getDoc(firestoreClientLib.doc(db, "products", prid));
+    if (uid != prSnapshot.data().ownerID) {
+        throw new errors.UnauthenticatedError("Unauthorized!");
+    }
+    const {
+        name,
+        priceDH,
+        description,
+        qty
+    } = req.body;
+
+    regexFloat = /^-?\d*\.?\d+$/;
+    regexInt = /^[0-9]+$/;
+    if ((priceDH && !priceDH.match(regexFloat))
+        || (qty && !qty.match(regexInt))) {
+        throw new errors.BadRequestError("Malformated request!");        
+    }
+    const newInfo = {
+        ...(name ? {name} : {}),
+        ...((typeof priceDH !== "undefined") ? {priceDH} : {}),
+        ...(description ? {description} : {}),
+        ...((typeof qty !== "undefined") ? {qty} : {})
+    };
+    await firestoreClientLib.updateDoc(firestoreClientLib.doc(db, "products", prid), newInfo);
+    res.status(httpStatus.StatusCodes.OK).send("Product patched successfully.");
+};
 
 const getOne = async (req, res) => {
     const prid = req.params.id;
@@ -14,5 +46,6 @@ const getOne = async (req, res) => {
 };
 
 module.exports = {
+    patchOne,
     getOne
 };
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const {authMiddleware} = require("../middleware/auth-middleware.js");
 const controller = require("../controllers/products.js");
 
-router.route("/:id").get(controller.getOne).patch(controller.patchOne);
+router.route("/:id").get(controller.getOne).patch(authMiddleware, controller.patchOne);
 
 module.exports = router;
