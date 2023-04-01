da04591b76d73a262f4082f86e140aaf55037e88
Rihab Dahdouh <rihabdahdouh271@gmail.com>
2023-04-01 12:43:18 +0000

    Initialized firebase frontend

--- _public/lib/main.dart
+++ _public/lib/main.dart
@@ -3,13 +3,15 @@ import 'package:get/get.dart';
 import 'package:news/screens/main/main_screen.dart';
 import 'package:news/screens/contactus/contact_us.dart';
 import 'constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
-
-
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
